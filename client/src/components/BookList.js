d6ec4061936422d5582dc959961fb82abafb6641
Neilson Gamble <neilson@scandy.co>
2022-03-22 13:48:46 -0600

    fetching within BookList comp

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -11,13 +11,16 @@ const BOOKS = gql`
 function BookList() {
   const { loading, error, data } = useQuery(BOOKS)
 
-  console.log(data)
+  const renderBooks = () => {
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error :(</p>
+
+    return data.books.map((book) => <li key={book.id}>{book.name}</li>)
+  }
 
   return (
     <div>
-      <ul id='book-list'>
-        <li>Book name</li>
-      </ul>
+      <ul id='book-list'>{renderBooks()}</ul>
     </div>
   )
 }
